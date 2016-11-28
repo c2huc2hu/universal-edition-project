6c0d6f8f048106415f8caaea952775342aa142df
ahsueh1996 <ahsueh1996@gmail.com>
2016-11-28 16:52:10 -0500

    Update Main.java
    Code for user input, draft... need to debug functionality

--- Main.java
+++ Main.java
@@ -4,13 +4,32 @@ public class Main {
 	
 	public static void main(String[] args) {
 		System.out.println("STARTING MAIN");
-		Button.waitForAnyPress();
 		Robot.gyroReset();
-		Robot.updateState(); 
+		Robot.updateState();
+		
+		int[] targetDoors = {-3, -2, -1, 1, 2, 3};
+		int[] targetPizza = {-1, 1};
+		int[] targetSel = {0,0};
+		int[] targetSize = {2,6};
+		int i = 0;
 		
 		// Code for initial cmd input
 // 		get target door
 // 		get target pizza
+		while (Button.ENTER.isUp()) {
+			if(Button.RIGHT.isDown()) i = Math.mod(i+1,2);
+			if(Button.LEFT.isDown()) i = Math.mod(i-1,2);
+			if(Button.UP.isDown()) {
+				targetSel[i] = Math.mod(targetSel[i]+1,targetSize[i]);
+			}
+			if(Button.UP.isDown()) {
+				targetSel[i] = Math.mod(targetSel[i]-1,targetSize[i]);
+			}
+			if ( i == 0) System.out.println(targetDoors[targetSel[i]]);
+			if ( i == 1) System.out.println(targetPizza[targetSel[i]]);
+		
+		}
+		
 		
 		Behavior[] behaviours = {
 			// insert your behavior instantiation here
