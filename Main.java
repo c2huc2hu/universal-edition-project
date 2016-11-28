020ff281ee5db642948cfb4ef1595afe0427fc28
ahsueh1996 <ahsueh1996@gmail.com>
2016-11-28 11:19:52 -0500

    Update Main.java
    Moved the ordering: behavior instantiation after main starts to allow for user inputs
    Commented/uncommented lines in the behavior instantiation, ready to test the delivery system

--- Main.java
+++ Main.java
@@ -3,22 +3,25 @@ import lejos.hardware.Button;
 public class Main {
 	
 	public static void main(String[] args) {
-		Behavior[] behaviours = {
-			// insert your behavior instantiation here
-			// we should try running combonations of behaviors too at some point to make
-			// sure integration works
-//		  new Avoider(30), // avoider arg is "too close"  
-//		  new Pathfinder(),
-// 		  new Delivery(50,0.312f)
-		}; 
-
 		System.out.println("STARTING MAIN");
 		Button.waitForAnyPress();
 		Robot.gyroReset();
 		Robot.updateState(); 
 		
 		// Code for initial cmd input
+// 		get target door
+// 		get target pizza
 		
+		Behavior[] behaviours = {
+			// insert your behavior instantiation here
+			// we should try running combonations of behaviors too at some point to make
+			// sure integration works
+		  new Avoider(30), // avoider arg is "too close", deliberately un-comment this to look at whether or not delivery will integrate with other things.
+//		  new Pathfinder(),
+		  new Delivery(50,0.312f) // arg: base speed when travelling on road, black line sonic value, need to put in target door
+		}; 
+
+
 		// Richard might want to put grab pizza here
 		while (Button.ESCAPE.isUp()) {
 			for(int i=0; i<behaviours.length; i++){
