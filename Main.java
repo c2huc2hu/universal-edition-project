079ebb4b1f931afaf548b6a00e920be8b244aea5
ROB301 <ROB301>
2016-12-01 15:21:08 -0500

    syntax fixes

--- Main.java
+++ Main.java
@@ -18,13 +18,13 @@ public class Main {
 // 		get target door
 // 		get target pizza
 		while (Button.ENTER.isUp()) {
-			if(Button.RIGHT.isDown()) i = Math.mod(i+1,3);
-			if(Button.LEFT.isDown()) i = Math.mod(i-1,3);
+			if(Button.RIGHT.isDown()) i = (i+1)%3;
+			if(Button.LEFT.isDown()) i = (i-1)%3;
 			if(Button.UP.isDown()) {
-				targetSel[i] = Math.mod(targetSel[i]+1,targetSize[i]);
+				targetSel[i] = (targetSel[i]+1)%targetSize[i];
 			}
 			if(Button.UP.isDown()) {
-				targetSel[i] = Math.mod(targetSel[i]-1,targetSize[i]);
+				targetSel[i] = (targetSel[i]-1)%targetSize[i];
 			}
 			if ( i == 0) System.out.println(targetDoors[targetSel[i]]);
 			if ( i == 1) System.out.println(targetPizza[targetSel[i]]);
@@ -38,13 +38,13 @@ public class Main {
 			// sure integration works
 		  new Avoider(30), // avoider arg is "too close", deliberately un-comment this to look at whether or not delivery will integrate with other things.
 //		  new Pathfinder(),
-		  new Delivery(50,0.312f,targetDoor[targetSel[1]]) // arg: base speed when travelling on road, black line sonic value, need to put in target door
+		  new Delivery(50,0.312f,targetDoors[targetSel[1]]) // arg: base speed when travelling on road, black line sonic value, need to put in target door
 		}; 
 
 
 		// Richard might want to put grab pizza here
 		while (Button.ESCAPE.isUp()) {
-			for(int i=0; i<behaviours.length; i++){
+			for(i=0; i<behaviours.length; i++){
 				if(behaviours[i].checkActive()) {
 					behaviours[i].act(-1);
 					break;
